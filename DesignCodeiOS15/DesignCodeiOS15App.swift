3b5506a27be24891355c5d484f638cf2e21e1ec1
Matthew Garlington <sbmatthewg@gmail.com>
2021-10-15 20:47:51 -0400

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by us-guest on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
