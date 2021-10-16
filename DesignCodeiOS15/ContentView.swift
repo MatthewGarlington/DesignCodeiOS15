77e2e739803a59da9e0b030466660cb0d6b41349
Matthew Garlington <sbmatthewg@gmail.com>
2021-10-15 21:52:07 -0400

    feat: Added Landing Page

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            VStack(alignment: .leading, spacing: 8.0) {
+                Image("Logo 2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 26, height: 26)
+                Text("SwiftUI for iOS 15")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("20 sections - 3 hours")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                Text("Design and Code a SwiftUI 3 app with custom layouts, animations and gestures using XCode 13, SF Symbols, Canvas, Concurency, Searchable, and a whole lot more")
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+            .padding(.all, 20)
+            .frame(height: 350)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+            
     }
 }
 
