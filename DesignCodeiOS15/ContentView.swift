a4910f7200823cfe408a9bfb2ad3857e7c2add35
Matthew Garlington <SBMatthewG@gmail.com>
2021-11-06 15:30:09 -0400

    feat: Layout Code and Preview

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -10,31 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
             VStack(alignment: .leading, spacing: 8.0) {
-//                Image("Logo 2")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width: 26, height: 26)
-//                    .cornerRadius(20)
+                Spacer()
+                Image("Logo 2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 26, height: 26)
+                    .cornerRadius(20)
                 Text("SwiftUI for iOS 15")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                Text("20 sections - 3 hours")
+                Text("20 sections - 3 hours".uppercased())
                     .font(.footnote)
                     .fontWeight(.semibold)
-                Text("Design and Code a SwiftUI 3 app with custom layouts, animations and gestures using XCode 13, SF Symbols, Canvas, Concurency, Searchable, and a whole lot more")
+                    .foregroundColor(.secondary)
+                Text("Design and Code a SwiftUI 3 app with custom layouts, animations and gestures using XCode 13, SF Symbols, Canvas, Concurrency, Searchable, and a whole lot more")
+                    .font(.footnote)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.secondary)
             }
-            .padding(.all, 20)
+            .padding(.all, 20) 
+            .padding(.vertical, 20)
             .frame(height: 350)
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+            .background(Color("Background"))
             .cornerRadius(30)
-            .shadow(radius: 20)
+            .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+            .padding(.horizontal, 20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
+
     }
 }
