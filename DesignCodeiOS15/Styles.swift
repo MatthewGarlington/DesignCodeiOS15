4aec12e776a8f3dae388dcef3f68eaa042d33b57
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-07 15:47:36 -0500

    feat: Added Styles file to hold resusable custom UI elements

---
+++ DesignCodeiOS15/Styles.swift
@@ -0,0 +1,20 @@
+//
+//  Styles.swift
+//  DesignCodeiOS15
+//
+//  Created by us-guest on 11/7/21.
+//
+
+import SwiftUI
+
+struct Styles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Styles_Previews: PreviewProvider {
+    static var previews: some View {
+        Styles()
+    }
+}
