7ed22dd2bb021fbe7db60ccad370316f83810b34
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-07 16:44:39 -0500

    style: Added resusable extension for the stroke style used through out the app

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
                     .cornerRadius(20)
                     .padding(9)
                     .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    .strokeStyle(cornerRadius: 16)
                 Text("SwiftUI for iOS 15")
                     .font(.largeTitle)
                     .fontWeight(.bold)
@@ -36,11 +37,9 @@ struct ContentView: View {
             .padding(.all, 20)
             .padding(.vertical, 20)
             .frame(height: 350)
-            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-            .overlay(RoundedRectangle(cornerRadius: 30, style: . continuous)
-                        .stroke(LinearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
-                        .blendMode(.overlay))
+            .strokeStyle()
             .padding(.horizontal, 20)
             .background(Image("Blob 1").offset(x: 250, y: -100))
             .overlay(Image("Illustration 5")
@@ -54,6 +53,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
             .previewInterfaceOrientation(.portrait)
 
     }
