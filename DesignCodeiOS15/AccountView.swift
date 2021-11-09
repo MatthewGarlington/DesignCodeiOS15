0727d8a36a3f305987e6b42d8d9d60342d5a9e36
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-08 21:35:34 -0500

    feat: git status cd

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -11,10 +11,36 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack {
+                Image(systemName: "person.crop.circle.badge.checkmark")
+                    .symbolVariant(.circle.fill)
+                    .font(.system(size: 32))
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.blue, .blue.opacity(0.5))
+                    .padding()
+                    .background(Circle().fill(.ultraThinMaterial))
+                    .background(
+                        Image(systemName: "hexagon")
+                            .symbolVariant(.fill)
+                            .foregroundColor(.blue)
+                            .font(.system(size: 200))
+                            .offset(x: -50, y: -100)
+                    )
+                    Text("Matthew Garlington")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.large)
+                        Text("United States")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
                 Section {
-                    Text("Settings")
-                    Text("Billing")
-                    Text("Help")
+                    Label("Settings", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
                 }
                 .listRowSeparator(.hidden)
                
