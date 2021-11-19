c905520046dcc032a09f1c95d617807f91d1d94a
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-18 21:52:04 -0500

    feat: Added Hexagon Canvas

---
+++ DesignCodeiOS15/HexigonView.swift
@@ -0,0 +1,20 @@
+//
+//  HexigonView.swift
+//  DesignCodeiOS15
+//
+//  Created by us-guest on 11/18/21.
+//
+
+import SwiftUI
+
+struct HexigonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HexigonView_Previews: PreviewProvider {
+    static var previews: some View {
+        HexigonView()
+    }
+}
